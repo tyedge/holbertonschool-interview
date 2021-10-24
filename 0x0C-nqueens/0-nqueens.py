1821a8a4dbe8c51afdac56fe247a60a68cacf0af
Ty Edge <tyedge.inc@gmail.com>
2021-10-24 21:26:06 +0000

    removed int casting from if-statement

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -4,18 +4,22 @@ a given value of N"""
 
 import sys
 
-N = int(sys.argv[1])
-onboard = []
+
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
 
-if type(int(sys.argv[1])) is not int:
+if type(sys.argv[1]) is not int:
     print("N must be a number")
     exit(1)
+
 if (int(sys.argv[1])) < 4:
     print("N must be at least 4")
     exit(1)
+
+
+N = int(sys.argv[1])
+onboard = []
 
 
 def nsolver(n):
